510f9392989b53233ff4591be2b8fd8f42d40314
Ma-chan <32949744+Ma-chan@users.noreply.github.com>
2018-11-10 17:02:02 +0900

    Create getMaxMin.java

---
+++ getMaxMin.java
@@ -0,0 +1,12 @@
+void getMaxMin(String[] args){
+           int max=arr[0];
+           int min=arr[1];
+           for(int i=0;i<arr.length;i++){
+               int v=arr[i];
+           if(v>max){
+               max=v;
+           }
+           if(v<min){
+               min=v;
+           }
+           }
