8f2fdf8273c8aff4ef225f878903756ad867a3d2
Ma-chan <32949744+Ma-chan@users.noreply.github.com>
2020-05-16 18:23:40 +0900

    Create java_card.java

---
+++ java_card.java
@@ -0,0 +1,10 @@
+import javacard.framework.*;
+import javacard.security.*;
+import javacardx.security.*;
+import javacardx.crypto.*;
+import java.io.*;
+import javacard.framework.service.*;
+
+public class Main extends Applet{
+
+}
